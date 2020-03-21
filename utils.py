dd4093436d96abc3f9192f0e6b46747175cf6f00
restaart <bazhanovr@yandex.ru>
2020-03-21 15:16:48 +0300

    added custom poll options

--- utils.py
+++ utils.py
@@ -2,8 +2,11 @@ from telebot import types
 
 
 def get_scrum_keyboard():
+    return get_custom_keyboard(['0.5', '1', '2', '3', '5', '8', '13'])
+
+def get_custom_keyboard(keys):
     keyboard = types.InlineKeyboardMarkup()
-    for key_label in ['0.5', '1', '2', '3', '5', '8', '13']:
+    for key_label in keys:
         key = types.InlineKeyboardButton(text=key_label, callback_data=key_label)
         keyboard.add(key)
     return keyboard
